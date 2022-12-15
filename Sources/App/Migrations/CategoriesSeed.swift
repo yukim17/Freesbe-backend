dec768416e8653df17cfbed3afd75ca3a0d1398b
egrishina <egrishina22@fed.idserve.net>
2022-12-15 12:07:58 +0100

    Seeds

--- Sources/App/Migrations/CategoriesSeed.swift
+++ Sources/App/Migrations/CategoriesSeed.swift
@@ -8,38 +8,43 @@
 import Fluent
 import Foundation
 
-struct CategoriesSeed: AsyncMigration {
+struct EventsSeed: AsyncMigration {
     
     func prepare(on database: Database) async throws {
+        let designCategory = Category(name: "Design", description: "", color: "")
+        let codeCategory = Category(name: "Code", description: "", color: "")
+        let socialCategory = Category(name: "Social", description: "", color: "")
+        let funCategory = Category(name: "Fun", description: "", color: "")
         let categories = [
-            Category(name: "Design", description: "", color: ""),
-            Category(name: "Code", description: "", color: ""),
+            designCategory,
+            codeCategory,
             Category(name: "Games", description: "", color: ""),
-            Category(name: "Social", description: "", color: ""),
+            socialCategory,
+            funCategory,
             Category(name: "Business", description: "", color: ""),
             Category(name: "Others", description: "", color: "")
         ]
         
+        try await categories.create(on: database)
+        
         let _ = categories.map { category in
             category.save(on: database)
         }.flatten(on: database.eventLoop)
-    }
-    
-    func revert(on database: Database) async throws {
-        try await Category.query(on: database).delete()
-    }
-}
-
-struct EventsSeed: AsyncMigration {
-    
-    func prepare(on database: Database) async throws {
+        
         let user1 = User(name: "John", surname: "Doe", username: "jdoe")
-        let user2 = User(name: "Angela", surname: "Doe", username: "adoe")
+        let user2 = User(name: "Ahmed", surname: "Mgua", username: "amgua")
         let user3 = User(name: "Will", surname: "Smith", username: "wsmith")
         try await [user1, user2, user3].create(on: database)
         
         let category = try await database.query(Category.self).first()
-        guard let userId = user1.id, let categoryId = category?.id else {
+        guard let user1Id = user1.id,
+              let user2Id = user2?.id,
+              let user3Id = user3?.id,
+              let designCategoryId = designCategory?.id,
+              let codeCategoryId = codeCategory?.id,
+              let funCategoryId = funCategory?.id,
+              let socialCategoryId = socialCategory?.id
+        else {
             throw FluentError.idRequired
         }
         let date = Date()
@@ -47,11 +52,13 @@ struct EventsSeed: AsyncMigration {
         dateFormatter.dateFormat = "d.MM.YYYY, hh:mm"
         let dateString = dateFormatter.string(from: date)
         let events = [
-            Event(title: "Some event 1", date: dateString, place: "Some place", categoryId: categoryId, organizerId: userId),
-            Event(title: "Some event 2", date: dateString, place: "Some place", categoryId: categoryId, organizerId: userId),
-            Event(title: "Some event 3", date: dateString, place: "Some place", categoryId: categoryId, organizerId: userId),
-            Event(title: "Some event 4", date: dateString, place: "Some place",categoryId: categoryId, organizerId: userId),
-            Event(title: "Some event 5", date: dateString, place: "Some place", categoryId: categoryId, organizerId: userId)
+            Event(title: "SwiftUI with Ahmed", description: "Hello everyone! It’s time for us to learn classes finally. Don’t worry, it’s not that difficult as you think. I will wait you!", date: dateString, place: "Collab 3-1", categoryId: codeCategoryId, organizerId: user2Id),
+            Event(title: "Cinemaholics", description: "", date: dateString, place: "Some place", categoryId: funCategoryId, organizerId: user1Id),
+            Event(title: "Russian cuisine", description: "", date: dateString, place: "Kitchen", categoryId: socialCategoryId, organizerId: user3Id),
+            Event(title: "Sketches", description: "", date: dateString, place: "Collab 1-3", categoryId: designCategoryId, organizerId: user1Id),
+            Event(title: "PixelArt Club", description: "", date: dateString, place: "Lab 2", categoryId: designCategoryId, organizerId: user3Id),
+            Event(title: "Italian language", description: "", date: dateString, place: "Collab 2-2", categoryId: socialCategoryId, organizerId: user1Id),
+            Event(title: "Trip to Rome", description: "", date: dateString, place: "Piazza Garibaldi", categoryId: funCategoryId, organizerId: user3Id)
         ]
         
         let _ = events.map { event in
